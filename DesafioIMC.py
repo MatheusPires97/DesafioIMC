47dae1128ffcb9b8315d77f9e54666654568ecbb
Matheus Pires <131575441+MatheusPires97@users.noreply.github.com>
2023-04-26 00:25:10 -0300

    update 2 casas decimais
    Feito a correção para exibição de 2 casas decimais no resultado.

--- DesafioIMC.py
+++ DesafioIMC.py
@@ -1,29 +1,29 @@
-#calculadora de imc
+# Calculadora de imc
 
 for imc in range(10000000):
   
-    altura = float(input('Digite sua altura: '))
-    peso = float(input('Digite seu pesso: '))
+    altura = float(input('Digite sua altura, "Ex: 1.82": '))
+    peso = float(input('Digite seu peso, "Ex: 80": '))
 
     calculo = peso / (altura * altura)
   
     if calculo < 18.5:
-      print(f'Voce está com IMC de {calculo} que é considerado magreza')
+      print(f'Voce está com IMC de {calculo:.2f} que é considerado magreza')
       
     elif calculo >= 18.5 and calculo < 25.0:
-      print(f'O seu IMC é de {calculo} que é considerado normal')
+      print(f'O seu IMC é de {calculo:.2f} que é considerado normal')
 
     elif calculo >= 25.0 and calculo < 30.0:
-      print(f'O seu IMC é de {calculo} que é considerado sobrepeso')
+      print(f'O seu IMC é de {calculo:.2f} que é considerado sobrepeso')
 
     elif calculo >= 30.0 and calculo < 35.0:
-      print(f'O seu IMC é de {calculo} que é considerado Obesidade nivel 1')
+      print(f'O seu IMC é de {calculo:.2f} que é considerado Obesidade nivel 1')
 
     elif calculo >= 35.0 and calculo < 38.0:
-      print(f'O seu IMC é de {calculo} que é considerado obesiade nivel 2')
+      print(f'O seu IMC é de {calculo:.2f} que é considerado obesiade nivel 2')
 
     elif calculo >= 38.0 and calculo < 100.0:
-      print(f'O seu IMC é de {calculo} que é considerado Obesidade nivel 3')
+      print(f'O seu IMC é de {calculo:.2f} que é considerado Obesidade nivel 3')
 
     else:
       print('digite o valor corretamente: ')
